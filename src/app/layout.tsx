7a0a05eebb6775417aacc376b5345b173ad2029f
João <arantesjoaov@gmail.com>
2023-07-16 20:36:39 -0300

    feat: initial commit/setting style guide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Sora } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const sora = Sora({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
