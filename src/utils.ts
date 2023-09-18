09f1ae43de0be84def605cae2a9fb227915729a6
João <arantesjoaov@gmail.com>
2023-09-18 16:06:01 -0300

    fix: removing some console.logs left behind

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,8 @@
 const scrollTo = (id: string) => {
-  console.log(id)
   const session = document.querySelector("#" + id)
 
   if (session !== null) {
     const topPos = session.getBoundingClientRect().top + window.scrollY
-    console.log(topPos)
     window.scrollTo({
       top: topPos - 20,
       behavior: "smooth",
